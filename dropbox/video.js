199215d2587ec18bb35adb30b7d2960a22abcc65
Joseph Nguyen <vietjew@gmail.com>
2015-03-10 21:26:39 -0700

    Fixed video player, removed grid outline

--- dropbox/video.js
+++ dropbox/video.js
@@ -1,4 +1,11 @@
+document.getElementById("play-button").addEventListener("click", function() {
+	togglePlayer();
+});
+
 function togglePlayer() {
 	document.getElementById("video-still").style.display = "none";
-	document.getElementById("video-player").style.display = "block";
+	document.getElementById("video").innerHTML = "<div class='col-12' id='video-player'>" +
+	"<video autoplay controls='controls' width='100%' height='500'>" + 
+	"<source src='assets/video_dfb_small.mov' type='video/mp4'>Your browser" +
+	" does not support the video tag.</video></div>"
 }

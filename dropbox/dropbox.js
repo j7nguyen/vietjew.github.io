c31d4d9d5e3006656fdee9381a4f6443c6881334
Joseph Nguyen <vietjew@gmail.com>
2015-04-03 16:09:05 -0700

    Updating JS

--- dropbox/dropbox.js
+++ dropbox/dropbox.js
@@ -10,10 +10,7 @@ $(window).scroll(function() {
 	hidden = $(".image-slide").hasClass("hidden");
 	inView = isVisible(".image-slide");
 	if ((inView && hidden) && $(window).scrollTop() >= 200) {
-		console.log("Images are in view");
 		$(".image-slide").toggleClass("hidden");
-	} else {
-		console.log("Go fuck yourself");
 	}
 });
 

268253995a7df1486ba9b15dc7cfecbc8b08c7ff
Joseph Nguyen <vietjew@gmail.com>
2015-05-07 01:04:54 -0700

    Firefox compatibility update

--- dropbox/video.js
+++ dropbox/video.js
@@ -4,6 +4,7 @@ function togglePlayer() {
 	$('#video-still').css("display","none");
 	$("#video").html("<div class='col-12' id='video-player'>" +
 	"<video autoplay controls='controls' width='100%' height='500'>" + 
-	"<source src='assets/video_dfb_small.mov' type='video/mp4'>Your browser" +
+	"<source src='assets/video_dfb_small.mov' type='video/mp4'><source src='assets/" +
+	"video_dfb_small.ogv' type='video/ogg'>Your browser" +
 	" does not support the video tag.</video></div>");
 }
